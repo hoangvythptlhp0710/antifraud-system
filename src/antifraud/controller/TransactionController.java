52d7efe5bb79e72afc7c1f839c2043c9d34eb4d1
hoangvythptlhp0710 <hoangvytb7891@gmail.com>
2023-07-13 09:58:10 +0700

    change transaction checking mechanism

--- src/antifraud/controller/TransactionController.java
+++ src/antifraud/controller/TransactionController.java
@@ -1,15 +1,15 @@
 package antifraud.controller;
 
+import antifraud.dto.Response;
+import antifraud.models.Status;
+import antifraud.models.Transaction;
 import antifraud.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/antifraud")
@@ -19,22 +19,21 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @PostMapping("/transaction")
-    public ResponseEntity makePurchase(@RequestBody Map<String, String> map) {
-        String str = map.get("amount");
+    public ResponseEntity<Response> makePurchase(@RequestBody Transaction transaction) {
         Long amount;
         try {
-            amount = Long.parseLong(str);
+            amount = Long.parseLong(transaction.getAmount().toString());
         } catch (NumberFormatException e) {
             amount = 0L;
         }
         if (amount <= 0) {
-            return new ResponseEntity(Map.of("amount", amount), HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().build();
         }
         else if (amount <= 200) {
-            return new ResponseEntity(Map.of("result", "ALLOWED"), HttpStatus.OK);
+            return ResponseEntity.ok(Response.builder().result(Status.ALLOWED).build());
         } else if (amount <= 1500) {
-            return new ResponseEntity(Map.of("result", "MANUAL_PROCESSING"), HttpStatus.OK);
+            return ResponseEntity.ok(Response.builder().result(Status.MANUAL_PROCESSING).build());
         }
-        return new ResponseEntity(Map.of("result", "PROHIBITED"), HttpStatus.OK);
+        return ResponseEntity.ok(Response.builder().result(Status.PROHIBITED).build());
     }
 }
